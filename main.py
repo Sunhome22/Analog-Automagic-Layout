70df2ac495ef18cdda9248e66c40ae5ccc4f2059
USERNAME <USERNAME@nordicsemi.no>
2024-09-16 16:57:03 +0200

    sh broken

--- main.py
+++ main.py
@@ -6,6 +6,5 @@ PROJECT_NAME_LONG = "JNW_BKLE_SKY130A"
 
 if __name__ == '__main__':
     print("[INFO]: Let's generate layout!")
-
     SPICEparser(project_name=PROJECT_NAME, project_dir=PROJECT_DIR)
 
