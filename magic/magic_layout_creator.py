0689b45471fee812ae899ec931235a577c2ba00c
USERNAME <USERNAME@nordicsemi.no>
2024-11-25 11:25:30 +0100

    Added license notice, cleaned up dead code, updated README

--- magic/magic_layout_creator.py
+++ magic/magic_layout_creator.py
@@ -1,4 +1,16 @@
-# TODO: Add copyright/license notice
+#======================================================================================================================#
+# Copyright (C) 2024 Bjørn K.T. Solheim, Leidulv Tønnesland
+#======================================================================================================================#
+# This program is free software: you can redistribute it and/or modify it under the terms of
+# the GNU General Public License as published by the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program.
+# If not, see <https://www.gnu.org/licenses/>.
+#======================================================================================================================#
 
 # ================================================== Libraries =========================================================
 import os
